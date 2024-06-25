1ea8b0973e5ae9e077bdb31c13e1bce35decd54e
Chamara-Wijepala <mr.worf777@gmail.com>
2024-06-25 16:46:01 +0530

    Complete task 7

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -16,14 +16,47 @@ const isValid = (username) => {
 };
 
 const authenticatedUser = (username, password) => {
-	//returns boolean
-	//write code to check if username and password match the one we have in records.
+	const filteredUsers = users.filter((user) => {
+		return user.username === username && user.password === password;
+	});
+
+	if (filteredUsers.length > 0) {
+		return true;
+	} else {
+		return false;
+	}
 };
 
 //only registered users can login
 regd_users.post('/login', (req, res) => {
-	//Write your code here
-	return res.status(300).json({ message: 'Yet to be implemented' });
+	const { username, password } = req.body;
+
+	if (!username || !password) {
+		return res
+			.status(404)
+			.json({ message: 'Username and Password are both required' });
+	}
+
+	if (!authenticatedUser(username, password)) {
+		return res
+			.status(208)
+			.json({ message: 'Invalid Login. Check username and password' });
+	}
+
+	const accessToken = jwt.sign(
+		{
+			data: password,
+		},
+		'access',
+		{ expiresIn: 60 * 60 }
+	);
+
+	req.session.authorization = {
+		accessToken,
+		username,
+	};
+
+	return res.status(200).send('User successfully logged in');
 });
 
 // Add a book review
